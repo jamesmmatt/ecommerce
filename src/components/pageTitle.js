8147bd1729953f6e643dda054fc2b670f3a300f2
Matthew James <jamesmmatt@gmail.com>
2019-03-12 09:53:56 -0500

    created a page title component and styled it and imported multiple fonts

---
+++ src/components/pageTitle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PageTitle extends Component {
+    render() {
+        const {className, title} = this.props;
+        return(
+            <div className={`${className} page-title`}>
+                Login
+            </div>
+        )
+    }
+}
+
+export default PageTitle;
